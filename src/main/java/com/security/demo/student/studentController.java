343232eb3cf5b55ecc807bfad43ba594c344c8e0
fareselheni <fareselheni21@gmail.com>
2022-09-19 14:24:28 +0200

    init api

--- src/main/java/com/security/demo/student/studentController.java
+++ src/main/java/com/security/demo/student/studentController.java
@@ -1,4 +1,28 @@
 package com.security.demo.student;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
 public class studentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "fares elheni"),
+            new Student(2, "assou ekkoto"),
+            new Student(3, "taye taiwo")
+    );
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId){
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("student" + studentId + "does not exist"));
+
+    }
 }
