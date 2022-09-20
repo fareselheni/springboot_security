1f1e188b506254c8a391f64e15b92dceb507be04
fareselheni <fareselheni21@gmail.com>
2022-09-20 12:01:16 +0200

    add pasword encoder

---
+++ src/main/java/com/security/demo/security/ApplicationUserRole.java
@@ -0,0 +1,6 @@
+package com.security.demo.security;
+
+public enum ApplicationUserRole {
+    STUDENT,
+    ADMIN
+}
--- src/main/java/com/security/demo/security/SecurityConfiguration.java
+++ src/main/java/com/security/demo/security/SecurityConfiguration.java
@@ -1,9 +1,14 @@
 package com.security.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -12,14 +17,38 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfiguration(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
+                        .antMatchers("/","index","/css/*","/js/*")
+                        .permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails FaresUser = User.builder()
+                .username("fares")
+                .password(passwordEncoder.encode("azerty12345"))
+                .roles("STUDENT")
+                .build();
+        UserDetails AdminUser = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("azerty12345"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(FaresUser,AdminUser);
     }
 
 }
